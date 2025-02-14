6f093c4dc53d0c4a9292332a95b0c5ab2e07fe03
michel <Michel-Scholer@gmx.de>
2025-02-14 08:55:39 +0100

    Revert "Kommentar geändert"
    This reverts commit b13abd333fd8fa2bd003c7c98f0bad921611ff10.

--- FunctionPlotter/MainForm.Designer.cs
+++ FunctionPlotter/MainForm.Designer.cs
@@ -32,14 +32,12 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
